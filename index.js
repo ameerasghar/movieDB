da463cd3d8f84528ed69321b6b74009f92e3f9cb
Ameer Asghar <ameerasghar@Ameers-PC.local>
2020-04-18 14:52:40 -0400

    changed

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// externals
 const express = require("express");
 const omdb = new (require("omdbapi"))("e051596");
+
+// internals
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("we live in this bitch");
+});
+
+app.get("/:search", async (req, res) => {
+  const search = req.params.search;
+  omdb
+    .search({
+      search: search,
+    })
+    .then((results) => {
+      res.send(results);
+    })
+    .catch(console.error);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
